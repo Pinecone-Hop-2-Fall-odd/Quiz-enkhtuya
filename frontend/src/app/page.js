d5fdc1c531bab32813541f81d70fec1c9d2ddb25
enkhtuya0511 <enkhtuya2051@gmail.com>
2023-11-15 19:29:41 +0800

    first commit

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import NavBar from './components/NavBar'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen bg-[#f6f7fb]">
+        <NavBar />
+    </main>
+  )
+}
